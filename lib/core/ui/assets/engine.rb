9c208ec7a8b3606ed593eef6a50ed487724a32c4
apinrdw <apinrdw@gmail.com>
2017-05-24 19:45:28 +0700

    update

--- lib/core/ui/assets/engine.rb
+++ lib/core/ui/assets/engine.rb
@@ -4,9 +4,6 @@ module Core
   module Ui
     module Assets
       class Engine < ::Rails::Engine
-        initializer 'core-ui.assets' do |app|
-          app.config.assets.paths << root.join('assets', 'stylesheets').to_s
-        end
       end
     end
   end
